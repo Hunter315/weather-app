5b3400349dd51dde7a3a5c3f15b0e22ada0c2ace
Hunter Smith <hunter.mckay.smith@gmail.com>
2024-08-11 19:53:34 -0600

    Initial commit

---
+++ test/openweather.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Openweather from '../lib/openweather-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/openweather-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Openweather.OpenweatherStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
